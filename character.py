f88334b995f4ebfce5c03e5b63748c3203906003
Jeong tae yeon <jtyk119@naver.com>
2023-10-29 14:34:57 +0900

    class Move, Run 추가

--- character.py
+++ character.py
@@ -1,6 +1,47 @@
 from pico2d import load_image, delay
 
 
+class Run:
+
+    @staticmethod
+    def enter(character, e):
+        pass
+
+    @staticmethod
+    def exit(character, e):
+        pass
+
+    @staticmethod
+    def do(character):
+        character.frame = (character.frame + 1) % 4
+        delay(0.07)
+
+    @staticmethod
+    def draw(character):
+        character.image.clip_draw(character.frame * 45, 90, 45, 45, character.x, 150, 135, 135)
+
+
+class Move:
+
+    @staticmethod
+    def enter(character, e):
+        pass
+
+    @staticmethod
+    def exit(character, e):
+        pass
+
+    @staticmethod
+    def do(character):
+        character.frame = (character.frame + 1) % 4
+        delay(0.07)
+
+    @staticmethod
+    def draw(character):
+        character.image.clip_draw(character.frame * 45, 450 - (135 * character.sword_position), 45, 45, character.x,
+                                  150, 135, 135)
+
+
 class Idle:
     @staticmethod
     def enter(character, e):
@@ -17,15 +58,18 @@ class Idle:
 
     @staticmethod
     def draw(character):
-        character.image.clip_draw(character.frame * 45, 495 - (135 * character.sword_position), 45, 45, character.x, 150, 135, 135)
+        character.image.clip_draw(character.frame * 45, 495 - (135 * character.sword_position), 45, 45, character.x,
+                                  150, 135, 135)
 
 
 class StateMachine:
     def __init__(self, character):
         self.character = character
-        self.cur_state = Idle
+        self.cur_state = Run
         self.transitions = {
             Idle: {},
+            Move: {},
+            Run: {},
         }
 
     def start(self):
@@ -51,7 +95,8 @@ class StateMachine:
 class Character:
     def __init__(self):
         self.x = 400
-        self.sword_position = 1                    # 검의 위치 / 상단 2 , 중단 1, 하단 0
+        self.sword_position = 1  # 검의 위치 / 상단 2 , 중단 1, 하단 0
+        self.dir = 0             # 캐릭터의 방향
         self.frame = 0
         self.image = load_image('character.png')
         self.state_machine = StateMachine(self)
