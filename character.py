b163fc104882a4a438ce813b2e5144bec3501bf3
Jeong tae yeon <jtyk119@naver.com>
2023-11-03 15:20:43 +0900

    캐릭터가 속력, 액션 프레임에 움직이도록 변경

--- character.py
+++ character.py
@@ -35,12 +35,11 @@ def D_up(e):
 
 
 PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 20.0
+RUN_SPEED_KMPH = 10.0
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
-# Boy Action Speed
-# fill here
+# character Action Speed
 TIME_PER_ACTION = 0.5
 ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
 FRAMES_PER_ACTION = 8
@@ -51,9 +50,9 @@ class Run:
     @staticmethod
     def enter(character, e):
         if right_down(e) or left_up(e):
-            character.dir = 1
+            character.dir, character.face_dir = 1, 1
         elif left_down(e) or right_up(e):
-            character.dir = -1
+            character.dir, character.face_dir = -1, 0
         pass
 
     @staticmethod
@@ -62,16 +61,16 @@ class Run:
 
     @staticmethod
     def do(character):
-        character.frame = (character.frame + 1) % 5
-        character.x += character.dir * 15
+        character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 5
+        character.x += character.dir * RUN_SPEED_PPS * 2.5 * game_framework.frame_time
         delay(0.05)
 
     @staticmethod
     def draw(character):
-        if character.dir == 1:
-            character.run_image.clip_draw(character.frame * 45, 0, 45, 45, character.x, 150, 135, 135)
-        elif character.dir == -1:
-            character.run_image.clip_composite_draw(character.frame * 45, 0, 45, 45, 0, 'h', character.x - 90, 150, 135, 135)
+        if character.face_dir == 1:
+            character.run_image.clip_draw(int(character.frame) * 45, 0, 45, 45, character.x, 150, 135, 135)
+        elif character.face_dir == 0:
+            character.run_image.clip_composite_draw(int(character.frame) * 45, 0, 45, 45, 0, 'h', character.x - 90, 150, 135, 135)
 
 
 class Move:
@@ -79,9 +78,9 @@ class Move:
     @staticmethod
     def enter(character, e):
         if right_down(e) or left_up(e):  # 오른쪽으로 Move
-            character.dir = 1
+            character.dir, character.face_dir = 1, 1
         elif left_down(e) or right_up(e):  # 왼쪽으로 Move
-            character.dir = -1
+            character.dir, character.face_dir = -1, 0
         if up_down(e) and character.sword_position < 2:
             character.sword_position += 1
         if down_down(e) and character.sword_position > 0:
@@ -93,17 +92,17 @@ class Move:
 
     @staticmethod
     def do(character):
-        character.frame = (character.frame + 1) % 4
-        character.x += character.dir * 5
+        character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
+        character.x += character.dir * RUN_SPEED_PPS * game_framework.frame_time
         delay(0.05)
 
     @staticmethod
     def draw(character):
         if character.dir == 1:
-            character.image.clip_draw(character.frame * 45, 405 - (135 * character.sword_position), 45, 45, character.x,
+            character.image.clip_draw(int(character.frame) * 45, 405 - (135 * character.sword_position), 45, 45, character.x,
                                       150, 135, 135)
         elif character.dir == -1:
-            character.image.clip_composite_draw(character.frame * 45, 405 - (135 * character.sword_position), 45, 45, 0,
+            character.image.clip_composite_draw(int(character.frame) * 45, 405 - (135 * character.sword_position), 45, 45, 0,
                                                 'h', character.x - 90, 150, 135, 135)
 
 
@@ -114,6 +113,7 @@ class Idle:
             character.sword_position += 1
         if down_down(e) and character.sword_position > 0:
             character.sword_position -= 1
+        character.dir = 0
 
     @staticmethod
     def exit(character, e):
@@ -121,16 +121,16 @@ class Idle:
 
     @staticmethod
     def do(character):
-        character.frame = (character.frame + 1) % 4
+        character.frame = (character.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 4
         delay(0.15)
 
     @staticmethod
     def draw(character):
-        if character.dir == 1:
-            character.image.clip_draw(character.frame * 45, 450 - (135 * character.sword_position), 45, 45, character.x,
+        if character.face_dir == 1:
+            character.image.clip_draw(int(character.frame) * 45, 450 - (135 * character.sword_position), 45, 45, character.x,
                                       150, 135, 135)
-        elif character.dir == -1:
-            character.image.clip_composite_draw(character.frame * 45, 450 - (135 * character.sword_position), 45, 45, 0,
+        elif character.face_dir == 0:
+            character.image.clip_composite_draw(int(character.frame) * 45, 450 - (135 * character.sword_position), 45, 45, 0,
                                                 'h', character.x - 90, 150, 135, 135)
 
 
@@ -142,7 +142,7 @@ class StateMachine:
             Idle: {right_down: Move, left_down: Move, left_up: Move, right_up: Move, up_down: Idle, down_down: Idle},
             Move: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle, up_down: Move, down_down: Move,
                    D_down: Run},
-            Run: {right_down: Run, left_down: Run, right_up: Idle, left_up: Idle, D_down: Run, D_up: Move},
+            Run: {right_down: Move, left_down: Move, right_up: Move, left_up: Move, D_down: Run, D_up: Move},
         }
 
     def start(self):
@@ -169,7 +169,8 @@ class Character:
     def __init__(self):
         self.x = 200
         self.sword_position = 1  # 검의 위치 / 상단 2 , 중단 1, 하단 0
-        self.dir = 1  # 캐릭터의 방향
+        self.face_dir = 1  # 캐릭터가 바라보는 방향  / 왼쪽 0, 오른쪽 1
+        self.dir = 0
         self.frame = 0
         self.image = load_image('character.png')
         self.run_image = load_image('Character_run.png')
