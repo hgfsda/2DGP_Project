14f88166220d5681bf037e130cf0d9f4daffd51a
Jeong tae yeon <jtyk119@naver.com>
2023-11-11 23:48:48 +0900

    게임 월드 충돌 검사 기본 세팅

--- game_world.py
+++ game_world.py
@@ -1,5 +1,5 @@
 objects = [[] for _ in range(4)]
-
+collision_pairs = {}
 
 def add_object(o, depth = 0):
     objects[depth].append(o)
@@ -20,10 +20,21 @@ def render():
             o.draw()
 
 
+def remove_collision_object(o):
+    for pairs in collision_pairs.values():
+        if o in pairs[0]:
+            pairs[0].remove(o)
+        if o in pairs[1]:
+            pairs[1].remove(o)
+    pass
+
+
 def remove_object(o):
     for layer in objects:
         if o in layer:
             layer.remove(o)
+            remove_collision_object(o)
+            del o
             return
     raise ValueError('Cannot delete non existing object')
 
@@ -31,3 +42,50 @@ def remove_object(o):
 def clear():
     for layer in objects:
         layer.clear()
+
+
+def collide_sword_body(a, b):
+    la, ba, ra, ta = a.sword_get_bb()
+    lb, bb, rb, tb = b.character_get_bb()
+
+    if la > rb: return False
+    if ra < lb: return False
+    if ta < bb: return False
+    if ba > tb: return False
+
+    return True
+
+
+def collide_sword_sword(a, b):
+    la, ba, ra, ta = a.sword_get_bb()
+    lb, bb, rb, tb = b.sword_get_bb()
+
+    if la > rb: return False
+    if ra < lb: return False
+    if ta < bb: return False
+    if ba > tb: return False
+
+    return True
+
+
+def add_collision_pair(group, a, b):
+    if group not in collision_pairs:
+        print(f'New group {group} added.')
+        collision_pairs[group] = [ [], [] ]
+    if a:
+        collision_pairs[group][0].append(a)
+    if b:
+        collision_pairs[group][1].append(b)
+
+
+def handle_collisions():
+    # 등록된 모든 충돌 상황에 대해서 충돌 검사 및 충돌 처리 수행
+    for group, pairs in collision_pairs.items():
+        for a in pairs[0]:
+            for b in pairs[1]:
+                if collide_sword_body(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
+                elif collide_sword_sword(a, b):
+                    a.handle_collision(group, b)
+                    b.handle_collision(group, a)
