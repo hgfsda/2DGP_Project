9ff759332b0eb5f6eadbbb36e7a90e35e9c96047
Jeong tae yeon <jtyk119@naver.com>
2023-11-15 15:33:30 +0900

    킬 수 확인

--- main_system.py
+++ main_system.py
@@ -7,6 +7,8 @@ class System:
         self.set_image = load_image('image\\set_check.png')
         self.play_time = 90
         self.start_time = get_time()
+        self.character_kill = 0
+        self.ai_kill = 0
 
     def update(self):
         if self.play_time > 0:
@@ -24,3 +26,11 @@ class System:
         # 적 세트 확인
         self.set_image.clip_draw(0, 300, 300, 300, 510, 550, 40, 40)
         self.set_image.clip_draw(0, 300, 300, 300, 560, 550, 40, 40)
+
+        # 주인공 킬 수 확인
+        self.num_image.clip_draw(100 * (self.character_kill % 100 // 10), 0, 100, 100, 270, 500, 30, 30)
+        self.num_image.clip_draw(100 * (self.character_kill % 10), 0, 100, 100, 300, 500, 30, 30)
+
+        # 적 킬 수 확인
+        self.num_image.clip_draw(100 * (self.ai_kill % 100 // 10), 0, 100, 100, 500, 500, 30, 30)
+        self.num_image.clip_draw(100 * (self.ai_kill % 10), 0, 100, 100, 530, 500, 30, 30)
