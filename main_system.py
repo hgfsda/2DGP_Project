1f5ace424c4fc99fe8ff78f8c7885a02d9cbc318
Jeong tae yeon <jtyk119@naver.com>
2023-11-15 15:26:14 +0900

    세트 확인 출력

--- main_system.py
+++ main_system.py
@@ -13,5 +13,14 @@ class System:
             self.play_time = 90 - get_time()
 
     def draw(self):
+        # 타이머
         self.num_image.clip_draw(100 * (int)(self.play_time % 100 // 10), 0, 100, 100, 360, 530, 80, 100)
         self.num_image.clip_draw(100 * (int)(self.play_time % 10), 0, 100, 100, 440, 530, 80, 100)
+
+        # 주인공 세트 확인
+        self.set_image.clip_draw(0, 300, 300, 300, 290, 550, 40, 40)
+        self.set_image.clip_draw(0, 300, 300, 300, 240, 550, 40, 40)
+
+        # 적 세트 확인
+        self.set_image.clip_draw(0, 300, 300, 300, 510, 550, 40, 40)
+        self.set_image.clip_draw(0, 300, 300, 300, 560, 550, 40, 40)
