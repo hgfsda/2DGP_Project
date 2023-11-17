cec3586ae4942ff371d99af4303bc5c0818a98c5
Jeong tae yeon <jtyk119@naver.com>
2023-11-17 22:47:12 +0900

    main_system에서 승리 확인 함수

--- main_system.py
+++ main_system.py
@@ -3,6 +3,8 @@ from pico2d import load_image, get_time
 character_kill = 0
 play_time = 91
 ai_kill = 0
+ch_win_check1, ch_win_check2 = 0, 0
+ai_win_check1, ai_win_check2 = 0, 0
 
 class System:
     def __init__(self):
@@ -28,12 +30,12 @@ class System:
         self.num_image.clip_draw(100 * (int)(play_time % 10), 0, 100, 100, 440, 530, 80, 100)
 
         # 주인공 세트 확인
-        self.set_image.clip_draw(0, 300, 300, 300, 290, 550, 40, 40)
-        self.set_image.clip_draw(0, 300, 300, 300, 240, 550, 40, 40)
+        self.set_image.clip_draw(0, 300 - (ch_win_check1 * 300), 300, 300, 290, 550, 40, 40)
+        self.set_image.clip_draw(0, 300 - (ch_win_check2 * 300), 300, 300, 240, 550, 40, 40)
 
         # 적 세트 확인
-        self.set_image.clip_draw(0, 300, 300, 300, 510, 550, 40, 40)
-        self.set_image.clip_draw(0, 300, 300, 300, 560, 550, 40, 40)
+        self.set_image.clip_draw(0, 300 - (ai_win_check1 * 300), 300, 300, 510, 550, 40, 40)
+        self.set_image.clip_draw(0, 300 - (ai_win_check2 * 300), 300, 300, 560, 550, 40, 40)
 
         # 주인공 킬 수 확인
         self.num_image.clip_draw(100 * (character_kill % 100 // 10), 0, 100, 100, 270, 500, 30, 30)
