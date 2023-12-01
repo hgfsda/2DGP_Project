82051271cdfdc4b43235bd69856f98a66e1911b1
Jeong tae yeon <134468880+hgfsda@users.noreply.github.com>
2023-12-01 12:30:52 +0900

    타이틀에서 위 아래로 움직이면 효과음 추가

--- title_mode.py
+++ title_mode.py
@@ -9,6 +9,8 @@ def init():
     global start_stage_image
     global start_word_image
     global title_bgm
+    global choose_menu
+    global confirmed_menu
     global check_y                # 시작 화면에서 start exit 표시해주는 화살표 위치
     global frame
     frame = 0
@@ -16,9 +18,12 @@ def init():
     start_stage_image = load_image('image\\start_stage.png')
     start_word_image = load_image('image\\start.png')
     title_bgm = load_music('sound\\title.mp3')
-    title_bgm.set_volume(22)
+    title_bgm.set_volume(12)
     title_bgm.repeat_play()
-    pass
+    choose_menu = load_wav('sound\\choose_menu.wav')
+    confirmed_menu = load_wav('sound\\confirmed_menu.wav')
+    choose_menu.set_volume(22)
+    confirmed_menu.set_volume(22)
 
 def finish():
     pass
@@ -30,7 +35,6 @@ def update():
     if frame <= -1300:
         frame = -100
     delay(0.03)
-    pass
 
 def draw():
     clear_canvas()
@@ -40,7 +44,6 @@ def draw():
     start_word_image.clip_draw(0, 10, 200, 60, 465, 150, 400, 120)  # exit
     start_word_image.clip_draw(200, 10, 30, 60, 165, 260 - (110 * check_y), 60, 120)  # 화살표 위치
     update_canvas()
-    pass
 
 def game_start_reset():
     main_system.total_ai_kill, main_system.total_ch_kill = 0, 0
@@ -50,6 +53,7 @@ def game_start_reset():
 def handle_events():
     global check_y
     global title_bgm
+    global choose_menu
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -57,6 +61,7 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and (event.key == SDLK_UP or event.key == SDLK_DOWN):
+            choose_menu.play()
             check_y = 1 - check_y
         elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_RETURN):
             if check_y == 0:
